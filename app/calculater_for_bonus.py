daa43835f129b5ad5214399adb9511dbd6abc7b0
aksi <a.k.sattarov@gmail.com>
2019-02-14 10:58:34 +0300

    Change the function code 'calculate_for_bonus' for the condition: -Calculate bonus amount baased on previous purchases

--- app/calculater_for_bonus.py
+++ app/calculater_for_bonus.py
@@ -1,28 +1,28 @@
-def calculate_for_bonus(purchase_amount):
+def calculate_for_bonus(purchase_amount, amount_previous_purchases):
 
     """
-    >>> calculate_for_bonus(999)
+    >>> calculate_for_bonus(999,0)
     0
 
-    >>> calculate_for_bonus(1_000)
+    >>> calculate_for_bonus(1_000, 1_100)
     50
 
-    >>> calculate_for_bonus(1668)
+    >>> calculate_for_bonus(1_668, 2_000)
     50
 
-    >>> calculate_for_bonus(4995)
+    >>> calculate_for_bonus(4_995, 15_000)
     200
 
-    >>> calculate_for_bonus(15199)
+    >>> calculate_for_bonus(15199, 15_001)
     1050
 
-    >>> calculate_for_bonus(49999)
-    3430
+    >>> calculate_for_bonus(49999, 150_001)
+    4900
 
-    >>> calculate_for_bonus(150_000)
-    10500
+    >>> calculate_for_bonus(150_000, 10)
+    7500
 
-    >>> calculate_for_bonus(169000)
+    >>> calculate_for_bonus(169_000, 160_000)
     16900
     """
 
@@ -31,26 +31,28 @@ def calculate_for_bonus(purchase_amount):
     number_bonus_on_begin_step = 50
     number_bonus_on_medium_step = 70
     number_bonus_on_high_step = 100
-    bottom_line = 1_000
+    bottom_line = 0
+    bottom_line_1 = 1000
     middle_border = 15_000
     upper_bound = 150_000
 
 
-    if purchase_amount < bottom_line:
+    if bottom_line < purchase_amount <  bottom_line_1:
         return 0
-    if bottom_line <= purchase_amount <= middle_border:
+
+    if bottom_line <= amount_previous_purchases <= middle_border:
         result = (purchase_amount // share) * number_bonus_on_begin_step
         return result
-    if middle_border <= purchase_amount <= upper_bound:
+    if middle_border <= amount_previous_purchases <= upper_bound:
         result1 = ((purchase_amount // share) * number_bonus_on_medium_step)
         return result1
-    if  purchase_amount > upper_bound:
+    if  amount_previous_purchases > upper_bound:
         result2 = ((purchase_amount // share) * number_bonus_on_high_step)
         return result2
 
-   # return "Thanks"
+    #return
 
-#print(calculate_for_bonus(16000))
+#print(calculate_for_bonus(169_000, 160_000))
 
 
 
